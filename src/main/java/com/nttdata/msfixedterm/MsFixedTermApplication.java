3bfce4d7ac41486b44896f095080af140023daa0
Mauricio Kevin Sulca Chavez <msulccha@emeal.nttdata.com>
2022-08-18 19:14:54 -0500

    primer

---
+++ src/main/java/com/nttdata/msfixedterm/MsFixedTermApplication.java
@@ -0,0 +1,13 @@
+package com.nttdata.msfixedterm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsFixedTermApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsFixedTermApplication.class, args);
+	}
+
+}
